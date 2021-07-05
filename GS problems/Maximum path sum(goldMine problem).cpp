3efbdd123114561a5bb095faa4406732a1c413d2
Shriyam Shrivastava <shriyam1026@gmail.com>
2021-07-05 13:10:21 +0530

    Change goldmine solution

--- GS problems/Maximum path sum(goldMine problem).cpp
+++ GS problems/Maximum path sum(goldMine problem).cpp
@@ -1,37 +1,30 @@
-class Solution {
-    int ans = INT_MIN;
-    void dfs(vector<vector<int>>&grid,vector<vector<bool>>&visited,int i,int j,int row,int col,int sum)
-    {
-        if(i<0||j<0||i>=row||j>=col||visited[i][j]==true)return;
-        visited[i][j] = true;
-        sum += grid[i][j];
-        ans = max(ans,sum);
-        if(i+1<row && !visited[i+1][j] && grid[i+1][j] != 0)
-            dfs(grid,visited,i+1,j,row,col,sum);
-        if(i-1>=0 && !visited[i-1][j] && grid[i-1][j] != 0)
-            dfs(grid,visited,i-1,j,row,col,sum);
-        if(j+1<col && !visited[i][j+1] && grid[i][j+1] != 0)
-            dfs(grid,visited,i,j+1,row,col,sum);
-        if(j-1>=0 && !visited[i][j-1] && grid[i][j-1] != 0)
-            dfs(grid,visited,i,j-1,row,col,sum);
-         visited[i][j] = false;
+int dfs(vector<vector<int>> & grid , int i, int j){
+        if(i<0 || i>=grid.size() || j<0 | j>=grid[0].size() || grid[i][j]==0)
+            return 0;
         
+        int curr=grid[i][j];
+        grid[i][j]=0;
+        int ans=0;
+        
+		//Checking all the four directions
+        ans=max(ans,dfs(grid,i+1,j));
+        ans=max(ans,dfs(grid,i-1,j));
+        ans=max(ans,dfs(grid,i,j-1));
+        ans=max(ans,dfs(grid,i,j+1));
+        
+        grid[i][j]=curr;   // Backtrack
+        
+        return ans+curr;
     }
-public:
+    
     int getMaximumGold(vector<vector<int>>& grid) {
-        int row = grid.size();
-        int col = grid[0].size();
-        vector<vector<bool>>visited(row,vector<bool>(col,0));
-       for(int i=0;i<row;i++)
-       {
-           for(int j=0;j<col;j++)
-           {
-               if(grid[i][j] != 0){
-               //visited.clear();
-               dfs(grid,visited,i,j,row,col,0);
-               }
-           }
-       }
+        int ans=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]>0){
+                    ans=max(ans,dfs(grid,i,j));
+                }
+            }
+        }
         return ans;
     }
-};
